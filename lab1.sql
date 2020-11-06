13289b06728addf1ef2b0729c4d9e1c0e967b9cd
Jalena Mathew <60174007+ceccs18c26@users.noreply.github.com>
2020-11-06 15:56:35 +0530

    Expt1

---
+++ lab1.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE LAB;
+
+use LAB;
+
+#a
+CREATE TABLE student(
+	sno INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	marks INT,
+	dept VARCHAR(20) NOT NULL
+	);
+
+#b
+ALTER TABLE student ADD COLUMN age INT NOT NULL;
+
+#C
+ALTER TABLE student MODIFY COLUMN dept VARCHAR(10);
+
+#d
+ALTER TABLE student DROP COLUMN marks;
+
+#e
+RENAME TABLE student TO students;
+
+#f
+TRUNCATE TABLE students;
+
+#g
+DROP TABLE students;
